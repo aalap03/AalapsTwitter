e8de7db8d45c692abf945103e1a3462d1c001bcc
aalap <apatel@e-cobalt.com>
2017-09-13 17:03:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aalap/aalapstwitter/AalapsTwitter.java
@@ -0,0 +1,18 @@
+package com.example.aalap.aalapstwitter;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.core.Twitter;
+
+/**
+ * Created by aalap on 2017-09-13.
+ */
+
+public class AalapsTwitter extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Twitter.initialize(getApplicationContext());
+    }
+}
